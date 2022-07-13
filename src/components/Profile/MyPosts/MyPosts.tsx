5f52521fbf98e029c9d76bd4e0f0a158daf7df31
Iuliia <sharipova.y.s@gmail.com>
2022-07-13 17:39:36 +0200

    add props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,22 +2,18 @@ import React from 'react';
 import style from './MyPosts.module.css';
 import Post from './Post/Post';
 
+
 const MyPosts = () => {
     return (
         <div>
             My posts
             <div>
-                New post
-            </div>
-            <div>
                 <textarea></textarea>
                 <button>Add post</button>
             </div>
             <div className={style.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message="It is my first message" likeCounts={10}/>
+                <Post message="What are you learning now&" likeCounts={15}/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import style from './Post.module.css';
 
-const Post = () => {
+type PostPropsType = {
+    message: string
+    likeCounts:number
+}
+
+export const Post = (props: PostPropsType) => {
     return (
         <div className={style.item}>
             <img src="https://static.independent.co.uk/2022/03/17/10/newFile.jpg?width=1200" alt="avatarka"/>
-            Post
+            {props.message}
+            <div><span>{props.likeCounts}</span></div>
         </div>
     );
 };
