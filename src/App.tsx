472a07ed2a1e1caaabe607dad149152226de7835
Iuliia <sharipova.y.s@gmail.com>
2022-08-11 15:11:58 +0200

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,29 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
     return (
-        <div className={'app-wrapper'}>
-            <Header/>
-            <Navbar/>
-            {/*<Profile/>*/}
-            <div className={'appWrapper-content'}>
-                <Dialogs/>
-            </div>
+        <BrowserRouter>
+            <div className={'app-wrapper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'appWrapper-content'}>
+                    <Routes>
+                        <Route path="/dialogs" element={<Dialogs/>}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/news" element={<News/>}/>
+                        <Route path="/music" element={<Music/>}/>
+                        <Route path="/settings" element={<Settings/>}/>
+                    </Routes>
+                </div>
 
-        </div>
+            </div>
+        </BrowserRouter>
     );
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,11 +4,11 @@ import style from './Navbar.module.css';
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href="src/components/Navbar/Navbar">Profile</a></div>
-            <div className={style.item}><a href="src/components/Navbar/Navbar">Messages</a></div>
-            <div className={style.item}><a href="src/components/Navbar/Navbar">News</a></div>
-            <div className={style.item}><a href="src/components/Navbar/Navbar">Music</a></div>
-            <div className={style.item}><a href="src/components/Navbar/Navbar">Settings</a></div>
+            <div className={style.item}><a href="/profile">Profile</a></div>
+            <div className={style.item}><a href="/dialogs">Messages</a></div>
+            <div className={style.item}><a href="/news">News</a></div>
+            <div className={style.item}><a href="/music">Music</a></div>
+            <div className={style.item}><a href="/settings">Settings</a></div>
 
         </nav>
     );
