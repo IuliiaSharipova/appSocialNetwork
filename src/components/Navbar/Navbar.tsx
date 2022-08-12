4247d90e19156720d03fe24a013bd8e0e8c446cd
Iuliia <sharipova.y.s@gmail.com>
2022-08-12 21:24:52 +0200

    change browser link without page reload - NavLink

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import style from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href="/profile">Profile</a></div>
-            <div className={style.item}><a href="/dialogs">Messages</a></div>
-            <div className={style.item}><a href="/news">News</a></div>
-            <div className={style.item}><a href="/music">Music</a></div>
-            <div className={style.item}><a href="/settings">Settings</a></div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/profile" >Profile</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/dialogs">Messages</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/news">News</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/music">Music</NavLink>
+            </div>
+            <div className={style.item}>
+                <NavLink className={navData => navData.isActive ? style.active : style.item} to="/settings">Settings</NavLink>
+            </div>
 
         </nav>
     );
