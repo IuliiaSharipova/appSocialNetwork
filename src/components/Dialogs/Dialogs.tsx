079c5f2abc52dc7a7562f754adc5c3c637dd37f9
Iuliia <sharipova.y.s@gmail.com>
2022-08-14 16:44:12 +0200

    dialog page layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import style from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-           Dialogs
-        </div>
+            <div className={style.dialogs}>
+                <div className={style.dialogsItem}>
+                    <div className={style.dialog}>
+                        Dima
+                    </div>
+                    <div className={style.dialog}>
+                        Sveta
+                    </div>
+                    <div className={style.dialog}>
+                        Ilia
+                    </div>
+                </div>
+                <div className={style.messages}>
+                    <div className={style.message}>Hi</div>
+                    <div className={style.message}>Yo</div>
+                    <div className={style.message}>How are you?</div>
+                </div>
+            </div>
     );
 };
 
