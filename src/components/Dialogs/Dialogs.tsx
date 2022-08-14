e44b1d8379524e379f2e989b5a5d50ed2ac21ab3
Iuliia <sharipova.y.s@gmail.com>
2022-08-14 18:37:48 +0200

    refactoring dialog's and profile's pages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import style from './Dialogs.module.css'
+import style from './Dialogs.module.css';
 import {NavLink} from 'react-router-dom';
+import {stringify} from 'querystring';
+
+export type DialogPropsType = {
+    name: string,
+    id: string
+};
+
+export const Dialog = (props: DialogPropsType) => {
+    return (
+        <div className={style.dialog}>
+            <NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
+        </div>
+    );
+};
+export type MessagePropsType = {
+    text: string
+};
+
+export const Message = (props: MessagePropsType) => {
+    return (
+        <div className={style.message}>{props.text}</div>
+    );
+};
 
 const Dialogs = () => {
     return (
-            <div className={style.dialogs}>
-                <div className={style.dialogsItem}>
-                    <div className={style.dialog}>
-                       <NavLink to='/dialogs/1'>Dima</NavLink>
-                    </div>
-                    <div className={style.dialog}>
-                        <NavLink to='/dialogs/2'>Sveta</NavLink>
-                    </div>
-                    <div className={style.dialog}>
-                        <NavLink to='/dialogs/3'>Ilia</NavLink>
-                    </div>
-                </div>
-                <div className={style.messages}>
-                    <div className={style.message}>Hi</div>
-                    <div className={style.message}>Yo</div>
-                    <div className={style.message}>How are you?</div>
-                </div>
+        <div className={style.dialogs}>
+
+            <div className={style.dialogsItem}>
+                <Dialog name="Dima" id="1"/>
+                <Dialog name="Sveta" id="2"/>
+                <Dialog name="Ilia" id="3"/>
             </div>
+
+            <div className={style.messages}>
+                <Message text="Hi"/>
+                <Message text="Yo"/>
+                <Message text="How are you?"/>
+            </div>
+
+        </div>
     );
 };
 
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -5,11 +5,13 @@ import Post from './Post/Post';
 
 const MyPosts = () => {
     return (
-        <div>
-            My posts
+        <div className={style.postsBlock}>
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <div><textarea></textarea></div>
+                <div>
+                    <button>Add post</button>
+                </div>
             </div>
             <div className={style.posts}>
                 <Post message="It is my first message" likesCount={10}/>
