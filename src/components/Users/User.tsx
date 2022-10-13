abe9e77667fc070a7c0bf788247cb3905c2ee3fa
Iuliia <sharipova.y.s@gmail.com>
2022-10-13 22:33:57 +0200

    moved get call to componentDidMount(54)

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -7,10 +7,10 @@ import userPhoto from '../../assets/images/user.jpg';
 type UserClassType = MapStatePropsType & MapDispatchPropsType
 
 class User extends React.Component<UserClassType> {
-    constructor(props:UserClassType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(res => {
-            this.props.setUsers(res.data.items)})}
+            this.props.setUsers(res.data.items)})
+    }
 
     render() {
         return (
